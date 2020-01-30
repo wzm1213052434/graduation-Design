43728f54df696ac1a4c755a295f33bf40fa8f38f
wzm1213052434 <1213052434@qq.com>
2020-01-30 13:17:59 +0800

    库模式修改

--- coreStore/js/index.js
+++ coreStore/js/index.js
@@ -1,3 +1,37 @@
+/**
+ * 模态框div
+ */
+let modal = 
+    `
+        <div class="mark-board">
+            <div class="modal-board">
+                <div class="modal-header">
+                    特殊符号列表
+                </div>
+                <div class="modal-content">    
+                </div>
+                <div class="modal-footer">
+                    <div class="modal-footer-content">
+                        <button id="">取消</button>
+                        <button >保存</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+let dataTable = 
+    `
+        <table>
+            <thead>
+                <th>序号</th>
+                <th>markdown</th>
+                <th>html</th>
+                <th>备注</th>
+                <th>操作</th>
+            </thead>
+            <tbody></tbody>
+        </table>
+    `;
 /*
     请求发送函数
 */
@@ -8,17 +42,15 @@ var getDatas = function() {
             type: "get",
             data: {},
             success: function(data) {
-                console.log("success" + data);
                 resolve(data);
             },
-            error: function(data) {
-                console.log("error" + data);
+            error: function(err) {
+                console.log("error" + err);
                 reject(data);
             }
         })
     })
 }
-
 /*
     字符串修整函数(修整表格中的字符串以方便显示)
 */
@@ -30,7 +62,6 @@ var clipStr = function(str) {
     let nail = str.substr(-20);
     return prev + "....." + nail;
 }
-
 /*
     总引用函数
 */
@@ -39,8 +70,8 @@ const specCharUIMod = {
         let basButton = $(`#${id}`);
         
     },
-    textAreaInit: function(id) {
-        let textArea = $(`#${id}`);
+    textAreaInit: function() {
+        $("body").append(modal);    
         let tbody = $(".modal-content > table > tbody");
         tbody.html("载入中");
         let data = getDatas().then((datas) => {
@@ -63,5 +94,16 @@ const specCharUIMod = {
         })
     }
 }
-specCharUIMod.textAreaInit("a");
+/**
+ * 将自定义函数挂在到jquery.
+ */
+for(let i in specCharUIMod) {
+    jQuery.fn.init.prototype[i] = specCharUIMod[i];
+}
 
+//主运行函数
+function main() {
+    $("body").textAreaInit();
+}
+
+main();
